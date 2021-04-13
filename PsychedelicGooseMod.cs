5566c85f1e41a6e6600b825fb806d28454787e44
idcs <idc.dts@gmail.com>
2021-04-13 10:47:31 +0100

    modified to use BepInEx

--- PsychedelicGooseMod.cs
+++ PsychedelicGooseMod.cs
@@ -1,110 +1,85 @@
-﻿using System;
-using Harmony;
-using System.IO;
-using System.Reflection;
+﻿using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
 using UnityEngine;
-using VortexHarmonyInstaller.ModTypes;
-using System.Collections.Generic;
 
 namespace PsychedelicGooseMod
 {
-    public class Main
+  public static class ModInfo
+  {
+    public const string GUID = "com.blacktreegaming.psychedelicgoose";
+    public const string Name = "Psychedelic Goose";
+    public const string Version = "1.0.1";
+  }
+
+  [BepInPlugin (ModInfo.GUID, ModInfo.Name, ModInfo.Version)]
+  [HarmonyPatch]
+  public class PsychedelicGoose : BaseUnityPlugin
+  {
+    private static Color m_currentColor = Color.red;
+    private static Color m_wantedColor = Color.blue;
+
+    private static float m_fFadeInTime = 1f;
+    private static float m_fProgress = 0.01f;
+
+    private static SkinnedMeshRenderer m_skin = null;
+
+    private void Awake()
     {
-        private static string DataPath { get; set; }
-        private static Assembly AssemblyResolver(object sender, ResolveEventArgs args)
-        {
-            string asmName = args.Name.Substring(0, args.Name.IndexOf(",")) + ".dll";
-            FileInfo[] libFiles = new DirectoryInfo(DataPath)
-                .GetFiles("*.dll", SearchOption.TopDirectoryOnly);
-            foreach (FileInfo dll in libFiles)
-            {
-                if (dll.Name == asmName)
-                {
-                    return Assembly.LoadFile(dll.FullName);
-                }
-            }
-
-            return null;
-        }
-
-        public static void RunPatch(VortexMod modInfo)
-        {
-            DataPath = modInfo.DataPath;
-            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolver;
-            try
-            {
-                var harmony = HarmonyInstance.Create("com.blacktreegaming.vortex.mod.test");
-                harmony.PatchAll(Assembly.GetExecutingAssembly());
-            }
-            catch (Exception exc)
-            {
-                modInfo.LogError("Harmony patching failed", exc);
-            }
-        }
+      new Harmony (ModInfo.GUID).PatchAll (typeof (PsychedelicGoose));
     }
 
-    [HarmonyPatch(typeof(Goose))]
-    [HarmonyPatch("Start")]
-    class Patch
+    [HarmonyPostfix]
+    [HarmonyPatch (typeof (Goose), "Start")]
+    private static void ChangeSkin(Goose __instance)
     {
-        private static Color m_currentColor = Color.red;
-        private static Color m_wantedColor = Color.blue;
-
-        private static float m_fFadeInTime = 1f;
-        private static float m_fProgress = 0.01f;
-
-        private static SkinnedMeshRenderer m_skin = null;
-
-        [HarmonyPostfix]
-        static void PostFix(Goose __instance)
-        {
-            m_skin = __instance.GetComponentsInChildren<SkinnedMeshRenderer>()[0];
-            Shader shader = Shader.Find("VertexLit");
-            Material mat = new Material(shader);
-            mat.SetColor("_Color", m_currentColor);
-            mat.SetFloat("_EmissionColor", m_currentColor.r * 10f);
-            //mat.SetTexture("_MainTex", null);
-            m_skin.material = new Material(mat);
-            StartRoutine(__instance);
-        }
-
-        public static void StartRoutine(Goose goose)
-        {
-            goose.StartCoroutine(ChangeWantedColor(goose));
-            goose.StartCoroutine(LerpColor());
-        }
-
-        private static IEnumerator<WaitForSeconds> ChangeWantedColor(Goose goose)
-        {
-            while (true)
-            {
-                if (goose.gooseHonker.justQuacked)
-                {
-                    m_currentColor = m_skin.material.color;
-                    m_fProgress = 0.01f;
-                    m_wantedColor = UnityEngine.Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
-                }
-
-                yield return null;
-            }
-        }
-
-        private static IEnumerator<WaitForSeconds> LerpColor()
-        {
-            while (true)
-            {
-                float increment = Time.deltaTime;
-                Color color = Color.Lerp(m_currentColor, m_wantedColor, m_fProgress / m_fFadeInTime);
-                if (m_fProgress < 1f)
-                {
-                    m_skin.material.SetColor("_Color", color);
-                    m_skin.material.SetFloat("_EmissionColor", color.r * 10f);
-                    m_skin.material.EnableKeyword("_EMISSION");
-                    m_fProgress += increment;
-                }
-                
-                yield return null;
-            }
-        }
+      m_skin = __instance.GetComponentsInChildren<SkinnedMeshRenderer> () [0];
+      Shader shader = Shader.Find ("VertexLit");
+      Material mat = new Material (shader);
+      mat.SetColor ("_Color", m_currentColor);
+      mat.SetFloat ("_EmissionColor", m_currentColor.r * 10f);
+      //mat.SetTexture("_MainTex", null);
+      m_skin.material = new Material (mat);
+      StartRoutine (__instance);
     }
+
+    private static void StartRoutine(Goose goose)
+    {
+      goose.StartCoroutine (ChangeWantedColor (goose));
+      goose.StartCoroutine (LerpColor ());
+    }
+
+    private static IEnumerator<WaitForSeconds> ChangeWantedColor(Goose goose)
+    {
+      while (true)
+      {
+        if (goose.gooseHonker.justQuacked)
+        {
+          m_currentColor = m_skin.material.color;
+          m_fProgress = 0.01f;
+          m_wantedColor = UnityEngine.Random.ColorHSV (0f, 1f, 0f, 1f, 0f, 1f);
+        }
+
+        yield return null;
+      }
+    }
+
+    private static IEnumerator<WaitForSeconds> LerpColor()
+    {
+      while (true)
+      {
+        float increment = Time.deltaTime;
+        Color color = Color.Lerp (m_currentColor, m_wantedColor, m_fProgress / m_fFadeInTime);
+        if (m_fProgress < 1f)
+        {
+          m_skin.material.SetColor ("_Color", color);
+          m_skin.material.SetFloat ("_EmissionColor", color.r * 10f);
+          m_skin.material.EnableKeyword ("_EMISSION");
+          m_fProgress += increment;
+        }
+
+        yield return null;
+      }
+    }
+  }
 }
